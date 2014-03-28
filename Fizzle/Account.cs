5ddfabaa1c64afeb7d4f462b2830c8b087a1ac39
Johannes Ekberg <uppfinnarn@gmail.com>
2014-03-29 00:47:03 +0100

    It can post things!

---
+++ Fizzle/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using TweetinviCore.Interfaces.oAuth;
+
+namespace Fizzle
+{
+	public class Account
+	{
+		public IOAuthCredentials credentials { get; set; }
+
+		public Account(String accessToken, String accessSecret)
+		{
+			credentials = TwitterCredentials.CreateCredentials(accessToken, accessSecret, ConfigurationManager.AppSettings["consumerKey"], ConfigurationManager.AppSettings["consumerSecret"]);
+		}
+
+		public void Post(string text)
+		{
+			Task.Factory.StartNew(() =>
+			{
+				TwitterCredentials.ExecuteOperationWithCredentials(credentials, () =>
+				{
+					Tweet.PublishTweet(text);
+				});
+			});
+		}
+	}
+}
--- Fizzle/MainWindow.xaml.cs
+++ Fizzle/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Tweetinvi;
+using TweetinviCore.Interfaces;
+using TweetinviCore.Interfaces.oAuth;
+using TweetinviCore.Interfaces.Credentials;
 
 namespace Fizzle
 {
@@ -20,9 +25,18 @@ namespace Fizzle
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		public Account account;
+		
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			account = new Account("1586652572-ZsDFzQbbeHVKDEtS82Y6gAg8QZ9XYnk198yg6Xx", "6lVkISwXMAi2BOSpos3klwMt1qTlCmuaZnkkGibOi2I2v");
+		}
+
+		private void SendButton_Click(object sender, RoutedEventArgs e)
+		{
+			account.Post(TweetText.Text);
 		}
 	}
 }
